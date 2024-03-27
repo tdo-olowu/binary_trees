8227bb5887b2ed71388f10b8e6a416fec15c1777
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-27 10:00:53 +0100

    files updated

--- binary_trees.h
+++ binary_trees.h
@@ -36,8 +36,6 @@ typedef struct binary_tree_s heap_t;
 
 
 /* functions */
-int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
