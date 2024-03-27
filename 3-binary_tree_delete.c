7b1a05aa4be36969e267445eed50fb3e768bb9e9
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-27 22:21:24 +0100

    files updated

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -18,10 +18,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	else
-	{
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
-	}
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
