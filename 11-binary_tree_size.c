6bac7b293bfbe93242cd5726855415f3faf148c0
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-27 23:40:24 +0100

    files updated

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,1 +1,20 @@
 #include "binary_trees.h"
+
+
+/**
+ * binary_tree_size - counts the number of nodes in a tree
+ * @tree: the subtree (or full tree)
+ * Return: how many nodes in the subtree?
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	const binary_tree_t *l;
+	const binary_tree_t *r;
+
+	if (tree == NULL)
+		return (0);
+	l = tree->left;
+	r = tree->right;
+
+	return (1 + binary_tree_size(l) + binary_tree_size(r));
+}
