c0b76aa7f4a240cae561cd8f31ff5aa543a52e31
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-28 01:37:27 +0100

    files updated

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,5 +7,15 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	return (0);
+	const binary_tree_t *l;
+	const binary_tree_t *r;
+
+	if (tree == NULL)
+		return (0);
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (0);
+	l = tree->left;
+	r = tree->right;
+
+	return (1 + binary_tree_nodes(l) + binary_tree_nodes(r));
 }
